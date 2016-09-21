16f3b73d6df6b1b618f2f7e7ed2d351594eab359
mangadinho <mangadinho@yahoo.fr>
2016-09-22 00:30:35 +0400

    quelque infos

---
+++ index.php
@@ -0,0 +1,10 @@
+<!Doctype html>
+      <html> 
+          <head>
+             <meta charset="utf-8"/>
+          </head>
+          <body>
+          bonjours les gars
+          </body>
+
+      </html>
