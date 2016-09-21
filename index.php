93c1b622c6ba6b7ae5b1de85512253206c418318
mangadinho <mangadinho@yahoo.fr>
2016-09-22 00:39:47 +0400

    le titre h1 vient juste d'etre ajoute

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
              <meta charset="utf-8"/>
           </head>
           <body>
-          bonjours les gars
+          bonjours les gars 
+	  <h1>vive le cameroun mes amis </h1>
           </body>
 
       </html>
